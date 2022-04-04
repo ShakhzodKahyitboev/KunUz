a7d93571e4bfec6c45dec031132eea16c496d5f9
Shakhzod <shahzod23yil@gmail.com>
2022-04-04 10:37:13 -0700

    first commit

---
+++ src/main/java/com/company/My_Kun_UzApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class My_Kun_UzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(My_Kun_UzApplication.class, args);
+	}
+
+}
